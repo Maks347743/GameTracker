727216912e235a080ec250de36288d63015e0cf4
Maks347743 <Maks347743@gmail.com>
2020-08-14 17:45:30 +0300

    GameListFragment dependency injection update (move this logic to component)

--- feature-gamelist/src/main/java/com/gmail/maks347743/feature_gamelist_impl/di/GameListFeatureComponent.kt
+++ feature-gamelist/src/main/java/com/gmail/maks347743/feature_gamelist_impl/di/GameListFeatureComponent.kt
@@ -1,6 +1,7 @@
 package com.gmail.maks347743.feature_gamelist_impl.di
 
 import com.gmail.maks347743.core_api.GameListFeatureNavigator
+import com.gmail.maks347743.core_api.ProviderHolder
 import com.gmail.maks347743.core_api.ProvidersAggregator
 import com.gmail.maks347743.core_api.Resources
 import com.gmail.maks347743.core_api.viewmodel.ViewModelFactoryProvider
@@ -9,19 +10,29 @@ import com.gmail.maks347743.feature_gamelist_impl.ui.GameListFragment
 import dagger.Component
 
 @FeatureScope
-@Component(dependencies = [GameListFeatureNavigator::class, Resources::class],
-modules = [ViewModelModule::class])
+@Component(
+    dependencies = [GameListFeatureNavigator::class, Resources::class],
+    modules = [ViewModelModule::class]
+)
 interface GameListFeatureComponent : ViewModelFactoryProvider {
 
     fun inject(gameListFragment: GameListFragment)
 
     companion object {
-        fun create(provider: ProvidersAggregator): GameListFeatureComponent {
+
+        private fun create(provider: ProvidersAggregator): GameListFeatureComponent {
             return DaggerGameListFeatureComponent
                 .builder()
                 .gameListFeatureNavigator(provider.gameListFeatureNavigator())
                 .resources(provider.resourceProvider())
                 .build()
+        }
+
+        fun injectFragment(fragment: GameListFragment): GameListFeatureComponent {
+            val component =
+                create((fragment.activity?.application as ProviderHolder).getProvidersAggregator())
+            component.inject(fragment)
+            return component
         }
     }
 
--- feature-gamelist/src/main/java/com/gmail/maks347743/feature_gamelist_impl/ui/GameListFragment.kt
+++ feature-gamelist/src/main/java/com/gmail/maks347743/feature_gamelist_impl/ui/GameListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.gmail.maks347743.core_api.ProviderHolder
 import com.gmail.maks347743.core_ui_utils.BaseFragment
 import com.gmail.maks347743.core_ui_utils.viewBinding
 import com.gmail.maks347743.feature_gamelist_impl.R
@@ -15,7 +14,7 @@ import com.gmail.maks347743.feature_gamelist_impl.ui.viewmodel.GameListViewModel
 class GameListFragment : BaseFragment<GameListViewModel>(R.layout.fragment_game_list) {
 
     private val component by lazy {
-        GameListFeatureComponent.create((activity?.application as ProviderHolder).getProvidersAggregator())
+        GameListFeatureComponent.injectFragment(this)
     }
     private val binding by viewBinding(FragmentGameListBinding::bind)
     override lateinit var viewModel: GameListViewModel
@@ -23,7 +22,6 @@ class GameListFragment : BaseFragment<GameListViewModel>(R.layout.fragment_game_
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        component.inject(this)
         viewModel =
             ViewModelProvider(this, component.viewModelFactory()).get(GameListViewModel::class.java)
 
